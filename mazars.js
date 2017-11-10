9944681fbea2ea19e88509fefce54c6cdd96fcab
simon REVEL <simon.revel@supelec.fr>
2017-11-10 23:01:59 +0100

    stable

--- mazars.js
+++ mazars.js
@@ -1,19 +1,22 @@
 var invaders = document.querySelectorAll("invader");
 var maz = document.querySelector("mazars");
 var mazpos = 0;
-var invapos = [100, 200, 300]
+var invapos = [100, 190, 280]
+var invaposreel = [100, 200, 300]
 var pas_invada = 0.75;
 var pas_maza =  0.25;
 var messageId = 0;
 var messageOn = false;
+
+
 
 window.addEventListener("load", function() {
 
 	window.addEventListener("keydown", function(evt) {
 		if(evt.keyCode == 39 && !messageOn) {
 			evt.preventDefault();
-			moveMaza(maz, pas_maza);
 			moveAllInvaders(invaders,pas_invada);
+			moveMaza(maz, pas_maza);
 			}
 
 		if(evt.keyCode == 37 && !messageOn) {
@@ -24,7 +27,7 @@ window.addEventListener("load", function() {
 
 		if (evt.keyCode == 13 && messageOn){
 
-			endMessage(messageId);
+			endMessage(invaders.length, messageId);
 
 		}
 		})
@@ -39,23 +42,25 @@ window.addEventListener("load", function() {
 		for(i = 0; i < invaderArr.length; i++)
 		{
 			invapos[i] = invapos[i] - pas;
+			invaposreel[i] = invaposreel[i] - pas;
 			invaderArr[i].style["transform"] = "translateX("+invapos[i]+"vw)";
-			if (invapos[i] - mazpos == 1)
+			if (Math.abs(invaposreel[i] - mazpos) <= 0.5)
 			{
 				messageId = i;
 				messageOn = true;
 				document.getElementById(i).style["display"] = "";
-				document.getElementById("mazaDisplay").src = "Cactus.jpg";
-
+				document.getElementById("mazaDisplayStatic").style["display"] = "";
+				document.getElementById("mazaDisplay").style["display"] = "none";
 			}
 		}
 	}
 
-	function endMessage(id){
+	function endMessage(invaderArrLength, id){
 
-		document.getElementById(id).style["display"] = "none";
 		messageOn = false;
-		document.getElementById("mazaDisplay").src = "assets/pika.gif";
+		document.getElementById(id).style["display"] = "none";
+		document.getElementById("mazaDisplayStatic").style["display"] = "none";
+		document.getElementById("mazaDisplay").style["display"] = "";
 
 	}
 
