14d26fc756f2027695a475739580b501b52fbf1e
simon REVEL <simon.revel@supelec.fr>
2017-11-11 18:23:56 +0100

    Fini !

--- mazars.js
+++ mazars.js
@@ -1,90 +1,113 @@
-var invaders = document.querySelectorAll("invader");
-var maz = document.querySelector("mazars");
-var mazpos = 0;
-var invapos = [100, 190, 280]
-var invaposreel = [100, 200, 300]
-var pas_invada = 0.75;
-var pas_maza =  0.25;
-var messageId = 0;
-var messageOn = false;
-var begin = true;
-var middle = false;
-
-
-
 window.addEventListener("load", function() {
+	var mazimg = document.querySelector("mazars img");
+	var invaders = document.querySelectorAll("invader");
+	var maz = document.querySelector("mazars");
+	var mazpos = getTranslateNum(maz);
+	var invapos = []
+	for(i = 0; i < invaders.length; i++)
+		invapos[i] = getTranslateNum(invaders[i]);
+	var pas_invada = 0.50;
+	var pas_maza =  0.50;
+	var messageId = 0;
+	var messageOn = false;
+	var begin = true;
+	var middle = false;
+	var walking = false;
+	var finish = false;
+
+
+	mazimg.setAttribute("src","mazarsImmo.png");
 
 	window.addEventListener("keydown", function(evt) {
 		if(evt.keyCode == 39 && !messageOn && middle) {
 			evt.preventDefault();
-			moveAllInvaders(invaders,pas_invada);
-			moveMaza(maz, pas_maza);
+			if(!walking) {
+				mazimg.setAttribute("src","mazarsKunDroite.gif");
+				walking = true;
 			}
-
+			moveAllInvaders(invaders,pas_invada);
+			if (mazpos <= 55 || finish){
+				moveMaza(maz, pas_maza);
+				console.log("mazbouge");
+			}
+			else
+				pas_invada = 0.75;
+		}
 		if(evt.keyCode == 37 && !messageOn && middle) {
 			evt.preventDefault();
+			if(!walking) {
+				mazimg.setAttribute("src","mazarsKunGauche.gif");
+				walking = true;
+			}
 			moveMaza(maz, -pas_maza);
 			moveAllInvaders(invaders, -pas_invada);
-			}
-
+		}
 		if (evt.keyCode == 13 && messageOn && middle){
-
+			mazimg.setAttribute("src","mazarsImmo.png");
 			endMessage(invaders.length, messageId);
-
 		}
-
 		if (evt.keyCode == 13 && begin){
-
+			mazimg.setAttribute("src","mazarsImmo.png");
 			goMiddle();
-
 		}
+	}, false);
 
-		})
-
-	function moveMaza(maza,pas){
-			mazpos = mazpos + pas;
-			maza.style["transform"] = "translateX("+mazpos+"vw)";
-			if (mazpos == 100){
-				middle = false;
-				document.querySelector("world").style["display"] = "none";
-				document.querySelector("end").style["display"] = "";
-			}
+	window.addEventListener("keyup", function(evt) {
+		evt.preventDefault();
+		if(walking && evt.keyCode == 39) {
+			mazimg.setAttribute("src","mazarsImmo.png");
+			walking = false;
 		}
+		if(walking && evt.keyCode == 37) {
+			mazimg.setAttribute("src","mazarsImmoRev.png");
+			walking = false;
+		}		
+	},false);
 
-	function moveAllInvaders(invaderArr,pas)
-	{
+	function moveMaza(maza,pas) {
+		mazpos = mazpos + pas;
+		maza.style["transform"] = "translateX("+mazpos+"vw)";
+		if (mazpos >= 90) {
+			middle = false;
+			document.querySelector("world").style["display"] = "none";
+			document.querySelector("end").style["display"] = "";
+		}
+	}
+
+	function moveAllInvaders(invaderArr,pas) {
 		for(i = 0; i < invaderArr.length; i++)
 		{
 			invapos[i] = invapos[i] - pas;
-			invaposreel[i] = invaposreel[i] - pas;
 			invaderArr[i].style["transform"] = "translateX("+invapos[i]+"vw)";
-			if (Math.abs(invaposreel[i] - mazpos) <= 0.5)
-			{
+			if (4.75 <= (invapos[i] - mazpos) && (invapos[i] - mazpos) <= 5) {
 				messageId = i;
 				messageOn = true;
 				document.getElementById(i).style["display"] = "";
-				document.getElementById("mazaDisplayStatic").style["display"] = "";
-				document.getElementById("mazaDisplay").style["display"] = "none";
+				mazaDisplay.setAttribute("src","mazarsImmo.png");
 			}
 		}
 	}
 
 	function endMessage(invaderArrLength, id){
-
 		messageOn = false;
 		document.getElementById(id).style["display"] = "none";
-		document.getElementById("mazaDisplayStatic").style["display"] = "none";
-		document.getElementById("mazaDisplay").style["display"] = "";
-
+		invaders[id].querySelector("img").setAttribute("src",id+"face.png");
+		if(id == invaderArrLength - 1){
+			finish = true;
+			console.log("finish");
+		}
 	}
 
 	function goMiddle(){
-
 		document.querySelector("world").style["display"] = "";
-		document.getElementById("begin").style["display"] = "none";
+		document.querySelector("firstScreen").style["display"] = "none";
 		middle = true;
 		begin = false;
+	}
 
+	function getTranslateNum(elt)
+	{
+		return parseFloat(elt.style["transform"].substring(11));
 	}
 
 },false);
